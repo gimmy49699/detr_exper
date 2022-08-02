0651891032d7fb216d85efce8c4af910225c6f3f
gimmy49699 <420607689@qq.com>
2022-08-03 00:12:40 +0800

    ckpt load

--- run.py
+++ run.py
@@ -182,7 +182,10 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_dict = model_without_ddp.state_dict()
+        pretrained_dict = {k: v for k, v in checkpoint['model'].items() if (k in model_dict)}
+        # model_without_ddp.load_state_dict(checkpoint['model'])
+        model_without_ddp.load_state_dict(pretrained_dict)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
